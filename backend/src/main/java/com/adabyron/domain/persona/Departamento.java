329c5339d0086865ab6e9fe3c8a5887fd2b12e43
Simon30804 <869800@unizar.es>
2026-03-01 20:39:41 +0100

    Iniciado desarrollo del dominio de Persona

--- backend/src/main/java/com/adabyron/domain/persona/Departamento.java
+++ backend/src/main/java/com/adabyron/domain/persona/Departamento.java
@@ -1,4 +1,38 @@
 package com.adabyron.domain.persona;
 
+
+/** Entidad - Departamento Universitario (REQ-B3)
+ *
+ * El departamento es una entidad inmutable, por lo que no vamos a exponer
+ * los métodos setters. Los dos departamentos son creados al iniciar
+ * el sistema, y nunca son modificados.
+ */
 public class Departamento {
+
+    private final DepartamentoId id;
+    private final String nombre;
+    private final String codigoSIGEUZ;
+
+    public Departamento(DepartamentoId id, String nombre, String codigoSIGEUZ) {
+        this.id = id;
+        this.nombre = nombre;
+        this.codigoSIGEUZ = codigoSIGEUZ;
+    }
+
+    /**
+     * Creamos las dos únicas instancias que pueden existir de Departamento, y las exponemos como constantes públicas.
+     * Son inmutables e idénticas durante toda la vida del sistema.
+     */
+    public static final Departamento INFORMATICA_INGENIERIA_SISTEMAS = new Departamento(DepartamentoId.INFORMATICA_INGENIERIA_SISTEMAS, "Informática e Ingeniería de Sistemas", "IIS");
+
+    public static final Departamento INGENIERIA_ELECTRONICA_COMUNICACIONES = new Departamento(DepartamentoId.INGENIERIA_ELECTRONICA_COMUNICACIONES, "Ingeniería Electrónica y Comunicaciones", "IEC");
+
+
+    public DepartamentoId getId() { return id;}
+    public String getNombre() { return nombre; }
+    public String getCodigoSIGEUZ() { return codigoSIGEUZ; }
+
+    @Override
+    public String toString() { return nombre; }
+
 }
--- backend/src/main/java/com/adabyron/domain/persona/PersonaRepository.java
+++ backend/src/main/java/com/adabyron/domain/persona/PersonaRepository.java
@@ -1,4 +1,29 @@
 package com.adabyron.domain.persona;
 
+import java.util.Optional;
+import java.util.List;
+
+
+/**
+ * Puerto de Salida - interfaz que el dominio expone para que el adaptador
+ * de infraestructura pueda implementar.
+ *
+ */
 public interface PersonaRepository {
+
+    Optional<Persona> findById(PersonaId id);
+
+    // REQ-B7 - El sistema debe permitir loguearse a un usuario
+    Optional<Persona> findByEmail(Email email);
+
+    // Para la UI de gestión de personas (será gestionado por el admin)
+    List<Persona> findAll();
+
+    List<Persona> findByRol(Rol rol);
+
+    List<Persona> findByDepartamento(DepartamentoId departamentoId);
+
+    void save(Persona persona);
+    void saveAll(List<Persona> personas);
+    boolean existsByEmail(Email email);
 }
