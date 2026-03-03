c6bb3529dfc994b77c5172b46fcfffe564c3e2ce
Simon30804 <869800@unizar.es>
2026-03-03 11:26:12 +0100

    Uso de JpaRepository

--- backend/src/main/java/com/adabyron/domain/persona/PersonaRepository.java
+++ backend/src/main/java/com/adabyron/domain/persona/PersonaRepository.java
@@ -2,6 +2,7 @@ package com.adabyron.domain.persona;
 
 import java.util.Optional;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
 /**
@@ -9,21 +10,29 @@ import java.util.List;
  * de infraestructura pueda implementar.
  *
  */
-public interface PersonaRepository {
+public interface PersonaRepository extends JpaRepository<Persona, PersonaId> {
 
-    Optional<Persona> findById(PersonaId id);
+    // Optional<Persona> findById(PersonaId id);
 
     // REQ-B7 - El sistema debe permitir loguearse a un usuario
-    Optional<Persona> findByEmail(Email email);
+    // No es necesario implementar este método, el JpaRepository ya lo implementa.
+    // Eso si, debo de declararlo para que el adaptador de infraestructura lo pueda usar, ya que el JpaRepository no lo expone.
+    //Optional<Persona> findByEmail(Email email);
+    void findByEmail(Email email);
+    void findById(Persona id);
 
-    // Para la UI de gestión de personas (será gestionado por el admin)
+    // Para la UI de gestión de personas (será gestionado por el admin) En principio no es necesario
+    // Ya lo implementa el JpaRepository.
     List<Persona> findAll();
 
     List<Persona> findByRol(Rol rol);
 
     List<Persona> findByDepartamento(DepartamentoId departamentoId);
 
-    void save(Persona persona);
-    void saveAll(List<Persona> personas);
     boolean existsByEmail(Email email);
+
+
+    // No es necesario implementar estos métodos, el JpaRepository ya los implementa.
+    //void save(Persona persona);
+    //void saveAll(List<Persona> personas);
 }
